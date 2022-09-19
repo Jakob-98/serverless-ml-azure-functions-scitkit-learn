65720a0730506f5355798a6ce770fde43ea939ce
Jakob Serlier <jakobserlier@microsoft.com>
2022-09-19 10:21:54 +0200

    init.py: better exceptions

--- src/SklearnModelFunction/__init__.py
+++ src/SklearnModelFunction/__init__.py
@@ -8,22 +8,22 @@ import azure.functions as func
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
     try: 
         req_body = req.get_body()
-    except ValueError: 
-        return func.HttpResponse("Bad request: no body",status_code=400)
-    
-    parsed_body = eval(req_body)
-    try: 
-        df = pd.DataFrame.from_dict(parsed_body)
-        df.drop('ArrDelay', axis=1, inplace=True)
-        df.drop('Carrier', axis=1, inplace=True)
-        
-
-        model = joblib.load(pathlib.Path(__file__).parent / 'model.joblib')
-        result = model.predict(df)
-        return func.HttpResponse(str(result))
+        parsed_body = eval(req_body)
+        df = pd.DataFrame.from_dict(parsed_body)   
 
     except ValueError:
         return func.HttpResponse("Bad request: invalid body structure",status_code=400)
+
+    except Exception as err:
+        return func.HttpResponse("Exception while parsing body: " + str(err), status_code=400)
+
+    try:
+        model = joblib.load(pathlib.Path(__file__).parent / 'model.joblib')
+        result = model.predict(df)
+
+        return func.HttpResponse(str(result))
+
+    except Exception as err:
+        return func.HttpResponse("Error while loading or predicting with model, exception: " + str(err), status_code=500)

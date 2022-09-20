2a1bc9d6635d10875937d942c9dffdb8bc49dc9c
Jakob Serlier <37184788+Jakob-98@users.noreply.github.com>
2022-09-20 13:27:31 +0200

    Squash commit for public repo purposes.

---
+++ src/SklearnModelFunction/__init__.py
@@ -0,0 +1,28 @@
+from sklearn.ensemble import RandomForestClassifier as RF
+import joblib
+import pandas as pd
+import pathlib
+
+import azure.functions as func
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    try: 
+        req_body = req.get_body()
+        parsed_body = eval(req_body)
+        df = pd.DataFrame.from_dict(parsed_body)   
+
+    except ValueError:
+        return func.HttpResponse("Bad request: invalid body structure",status_code=400)
+
+    except Exception as err:
+        return func.HttpResponse("Exception while parsing body: " + str(err), status_code=400)
+
+    try:
+        model = joblib.load(pathlib.Path(__file__).parent / 'model.joblib')
+        result = model.predict(df)
+
+        return func.HttpResponse(str(result))
+
+    except Exception as err:
+        return func.HttpResponse("Error while loading or predicting with model, exception: " + str(err), status_code=500)
---
+++ terraform/provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.23.0"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+provider "azurerm" {
+  features {}
+  
+  skip_provider_registration = true  
+}

f0d1018a0717c539475dec8df54b458c302a704b
Marco van Kimmenade <BlueBasher@users.noreply.github.com>
2022-09-20 09:52:18 +0200

    Update __init__.py

--- src/TestFunction/__init__.py
+++ src/TestFunction/__init__.py
@@ -8,5 +8,5 @@ import azure.functions as func
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    return func.HttpResponse("huzza!", status_code=200)
+    return func.HttpResponse("1 huzza!", status_code=200)
     
